da8bdc899b903a03fb0b0bf8f22bd2bef14b1eb1
Enrique Marina <eherraiz@myuax.com>
2022-11-11 17:27:45 +0100

    n

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\eherr\OneDrive\Escritorio\UAX\3er Curso\Desarrollo Orientado a Objetos\Django-Server\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
